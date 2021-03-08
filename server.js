23097f3af676c8acb657e7eaa06f4d940dd13091
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-03-08 18:32:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const app = express();
 
 // Persistência
 mongoose.connect(
-	'mongodb+srv://luizmurakami:' +
+	'mongodb+srv://dotask:dotask@dotask.x4hcs.mongodb.net/test',
+	{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false, useUnifiedTopology: true  }
+);
+/*
+'mongodb+srv://luizmurakami:' +
 		process.env.PASSWORD +
 		'@cluster0-usoip.mongodb.net/agrostation?retryWrites=true&w=majority',
 	{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true }
-);
-
+*/
 // Configurar a aplicação para usar o Body-Parser
 app.use(bodyParser.urlencoded([{ extended: false }]));
 app.use(bodyParser.json());
@@ -21,13 +24,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Porta da aplicação
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Rotas
 var indexRoute = require('./src/routes/index-route');
 var regRoute = require('./src/routes/register-route');
 var authRoute = require('./src/routes/authenticate-route');
 var taskRoute = require('./src/routes/task-route');
+var userRoute = require('./src/routes/user-route');
 
 // Rota para Index
 app.use('/api', indexRoute);
@@ -40,5 +44,8 @@ app.use('/api/authenticate', authRoute);
 
 // Rota para Propriedade
 app.use('/api/task', taskRoute);
+
+// Rota para Usuários
+app.use('/api/user', userRoute)
 
 app.listen(PORT, () => console.log('API Running in Port: ' + PORT));
