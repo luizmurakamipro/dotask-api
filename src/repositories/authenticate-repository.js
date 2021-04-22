84373e2058b9cf4dee310950a550c07f6caa39ae
Daniel Moya da Silva <danielm.silva@univem.edu.br>
2021-04-22 18:41:39 -0300

    Retornando nome do usuário

--- src/repositories/authenticate-repository.js
+++ src/repositories/authenticate-repository.js
@@ -4,29 +4,35 @@ require("dotenv-safe").config();
 
 exports.authenticate = async (email, password) => {
     const user = await User.findOne({ email }).select('+password');
-        
+
     if (!user) {
         console.log("Usuário inexistente");
         throw ({
-            status: 404, 
+            status: 404,
             code: "Usuário não encontrado",
-            message: "Usuário inexistente"});  
+            message: "Usuário inexistente"
+        });
     }
     if (user.email !== email) {
         console.log("Email incorreto");
         throw ({
-            status: 404, 
+            status: 404,
             code: "Usuário não encontrado",
-            message: "Email incorreto"});  
+            message: "Email incorreto"
+        });
     }
     if (!user.validPassword(password)) {
         console.log("Senha incorreta");
         throw ({
-            status: 404, 
+            status: 404,
             code: "Usuário não encontrado",
-            message: "Senha incorreta"});  
+            message: "Senha incorreta"
+        });
     }
-    console.log('USER',user);
+    console.log('USER', user);
     const id = user._id;
-    return jwt.sign({id}, process.env.SECRET, {expiresIn:86400});
+    let token = jwt.sign({ id }, process.env.SECRET, { expiresIn: 86400 });
+    console.log(user.name);
+    let obj = { token: token, userName: user.name }
+    return obj;
 }
